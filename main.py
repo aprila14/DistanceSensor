c12969f4d6df7cf78a02e2fc6ec521beeda24600
kevin marty <martyk@mit.edu>
2019-12-12 18:43:59 -0500

    first try

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from RPi import GPIO
 from time import sleep
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+def animate(i,xs,ys):
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(temp_c)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
 
 clk = 17
 dt = 18
@@ -11,8 +33,13 @@ GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter = 0
 clkLastState = GPIO.input(clk)
 
-try:
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+xs = []
+ys = []
+temp_c = 0
 
+try:
         while True:
                 clkState = GPIO.input(clk)
                 dtState = GPIO.input(dt)
@@ -23,6 +50,10 @@ try:
                                 counter -= 1
                 clkLastState = clkState
                 len = counter*0.05
-                print(len)
+                temp_c = len
+                print(temp_c)
+                ani = animation.FuncAnimation(fig, animate,fargs=(len, ys), interval=1000)
+                plt.show()
 finally:
         GPIO.cleanup()
+
