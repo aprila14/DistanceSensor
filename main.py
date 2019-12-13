86e8c2ed9e009f15e8b2d0d91f5a9c2c85a1dd0a
kevin marty <martyk@mit.edu>
2019-12-12 19:51:34 -0500

    delete matplot

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 #from RPi import GPIO
 from time import sleep
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import time
-
 import msvcrt
 
 
-
-
-def animate(i,xs,ys):
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(temp_c)
-
-    # Limit x and y lists to 20 items
-    xs = xs[-20:]
-    ys = ys[-20:]
-
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('TMP102 Temperature over Time')
-    plt.ylabel('Temperature (deg C)')
-
 #open a file
-file1 = open("datavalues.txt","a") 
-listen()
+file1 = open("datavalues.txt","r+") 
 
 
 clk = 17
@@ -48,6 +23,9 @@ ax1 = fig.add_subplot(1,1,1)
 xs = []
 ys = []
 temp_c = 0
+
+len = 0
+
 
 start = True
 try:
@@ -67,16 +45,12 @@ try:
                                 counter -= 1
                 clkLastState = clkState
                 len = counter*0.05
-                temp_c = len
-                print(temp_c)
-                ani = animation.FuncAnimation(fig, animate,fargs=(len, ys), interval=1000)
-                plt.show()
+                file.write(len+";")
+
                 
                 
 finally:
         GPIO.cleanup()
-
-
 
 file1.close() 
 
