447b8a36a32d33d5a526d8aa53eae49ad0217345
kevin marty <martyk@mit.edu>
2019-12-12 20:09:34 -0500

    with keyboard as module

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#from RPi import GPIO
+from RPi import GPIO
 from time import sleep
 import time
-import msvcrt
+import keyboard  # using module keyboard
 
 
 #open a file
@@ -18,39 +18,29 @@ GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter = 0
 clkLastState = GPIO.input(clk)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-xs = []
-ys = []
-temp_c = 0
 
-len = 0
-
-
-start = True
-try:
-        while start == True:
-            if msvcrt.kbhit():
-                key_stroke = msvcrt.getch()
-                if str(key_stroke[0]) == "48":
-                    print("start switch to false")
-                    start = False
-                #print(str(key_stroke[0]))   # will print which key is pressed
-                clkState = GPIO.input(clk)
-                dtState = GPIO.input(dt)
-                if clkState != clkLastState:
-                        if dtState != clkState:
-                                counter += 1
-                        else:
-                                counter -= 1
-                clkLastState = clkState
-                len = counter*0.05
-                file.write(len+";")
+while True:
+    print("start")
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('A'):  # if key 'q' is pressed 
+            print('You Pressed q Key!')
+            break  # finishing the loop
+        
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        if clkState != clkLastState:
+                if dtState != clkState:
+                        counter += 1
+                else:
+                        counter -= 1
+        clkLastState = clkState
+        len = counter*0.05
+        file.write(len+";")
+    
+    except:
+        break  # if user pressed a key other than the given key the loop will break
 
                 
-                
-finally:
-        GPIO.cleanup()
-
+GPIO.cleanup()
 file1.close() 
 
