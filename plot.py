187341a23c5ef493dd3c7d1d7f8bbfc0a0e507ad
aprila14 <36075569+aprila14@users.noreply.github.com>
2020-11-02 15:56:59 -0500

    new plot

--- plot.py
+++ plot.py
@@ -3,17 +3,26 @@ import csv
 
 x = []
 y = []
+n = 10 #it takes every n-th values. n=1 is full resoltuion
 
 with open('datavalues.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=';')
-    for row in plots:
-        x.append(int(row[0]))
-        y.append(int(row[1]))
-print(x)
-print(y)
-plt.plot(x,y, label='Loaded from file!')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out')
+    data = csv.reader(csvfile, delimiter=';')
+    j=0
+    k=0
+    for row in data:
+        print("number of row: {}".format(len(row)))
+        for i in row:
+            j=j+1
+            #print("j = {}".format(j))
+            if (j%n)==0:
+                k=k+1
+                #print("k = {}".format(k))
+                x.append(k)
+                y.append(float(i))
+
+plt.scatter(x,y,1, color='black',label='Federweg')
+plt.xlabel('Time [ms]')
+plt.ylabel('Federweg [mm]')
+plt.title('Federweg vs. Zeit')
 plt.legend()
 plt.show()
